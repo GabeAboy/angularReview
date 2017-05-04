f0b3733523902965d52294491307c95fe5684052
Merksue <gabeaboy@gmail.com>
2017-05-04 14:46:59 -0400

    watchListAng

---
+++ isolatedScope/TemplateIsolateScopeDirective.js
@@ -0,0 +1,39 @@
+var candidateApp = angular.module('candidates', []);
+
+candidateApp
+.controller('CandidateController', ['$scope', function($scope) {
+    $scope.donald = {
+      name: 'Donald Trump',
+      party: 'Republican',
+      spouse: 'Melania Trump'
+    };
+    $scope.hillary = {
+        name: 'Hillary Clinton',
+        party: 'Democrat',
+        spouse: 'Bill Clinton'
+    };
+}])
+.directive('loonyCandidate', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            info: '=candidateInfo',
+            candidateInfo: '='
+        },
+        template: 'Name: {{info.name}} ({{info.party}}) Spouse: {{candidateInfo.spouse}}'
+    };
+});
+
+var button = document.getElementById('clickMe');
+
+function buttonClicked () {
+    alert('the button was clicked');
+}
+
+button.addEventListener('click', buttonClicked);
+
+function timerComplete () {
+    alert('timer complete');
+}
+
+setTimeout(timerComplete, 2000);
